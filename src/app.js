dffd42f7ed53c5c0d2b18de38255129ae9e600da
Huynh Hoang <huynh.hoang@menlosecurity.com>
2022-02-07 10:59:40 +0700

    test account

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ require('./utils/database')
 const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
-
+// test github
 app.listen(port, () => console.log(`Server is running at ${port}`))
