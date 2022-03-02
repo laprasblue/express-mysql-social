69a8c5671e107b2eea952ca3eca3ef0769118e2f
Laprasblue <lapras.blue.97@gmail.com>
2022-03-03 01:50:39 +0700

    setup swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
 require('dotenv').config()
 require('./utils/database')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'b',
+      description: 'Test api',
+      version: '1.0.0',
+      contact: {
+        name: 'Huynh Hoang',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: [`${__dirname}/utils/swagger.js`],
+}
+
+const specs = swaggerJsdoc(options)
 
 const cors = require('cors')
 const morgan = require('morgan')
@@ -8,6 +46,7 @@ const port = process.env.PORT || 5000
 const express = require('express')
 const router = require('./routes')
 const app = express()
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use(helmet())
 app.use(cors())
 app.use(morgan('tiny'))
---
+++ src/utils/swagger.js
@@ -0,0 +1,29 @@
+/**
+ * @swagger
+ * /api/v1/user/profile:
+ *    get:
+ *      summary: 'user heelo'
+ *      tags: [Book]
+ *      description:  Endpoint for everything
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *        200:
+ *          description: successful
+ *        403:
+ *          description: successful
+ *
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  - name: User
+ *    description: the user
+ *  - name: Book
+ *    description: the user
+ *  - name: Users
+ *    description: the user
+ *  - name: Users1
+ *    description: the user
+ */
